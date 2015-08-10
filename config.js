8af3ed4107d0fca2013aada02cc2c3a3774bb9aa
LI LIN <r2space@gmail.com>
2015-08-10 20:10:36 +0800

    添加模板

--- config.js
+++ config.js
@@ -2,10 +2,10 @@
  * app 设置
  */
 module.exports = {
-  app_domain: '',  //应用标识
-  db_host   : '',            //本地运行时的数据库地址
-  db_port   : '',         //本地运行时的数据库端口
-  db_user   : '',          //数据库用户名
-  db_pass   : '',          //数据库密码
-  app_port  : 45678            //本地运行时的端口
+  app_domain: '56648deee144',     //应用标识
+  db_host   : 'db.starwall.org',  //本地运行时的数据库地址
+  db_port   : '57017',            //本地运行时的数据库端口
+  db_user   : 'r2space',          //数据库用户名
+  db_pass   : 'ranbow',           //数据库密码
+  app_port  : 45677               //本地运行时的端口
 };
