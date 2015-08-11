fa7de560ddbd9edf6157ff34a66578cecdbfda84
LI LIN <r2space@gmail.com>
2015-08-11 11:07:45 +0800

    生成画面样例

---
+++ public/static/scripts/index.js
@@ -0,0 +1,37 @@
+
+jQuery(document).ready(function($) {
+
+  $("#content").on("click", "img", function() {
+
+    var type = $(this).attr("alt");
+
+    if (type == "Blog") {
+      window.location = "/blog.html";
+    }
+
+    if (type == "Calendar") {
+      window.location = "/calendar.html";
+    }
+
+    if (type == "Card") {
+      window.location = "/card.html";
+    }
+
+    if (type == "Contact") {
+      window.location = "/contact.html";
+    }
+
+    if (type == "Gallery") {
+      window.location = "/gallery.html";
+    }
+
+    if (type == "Portfolio") {
+      window.location = "/portfolio.html";
+    }
+
+    if (type == "Soon") {
+      window.location = "/soon.html";
+    }
+  });
+
+});
