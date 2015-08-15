731c2c4ee7ec31f7cb215b0eeb20fd4bb9a6a9dd
LI LIN <r2space@gmail.com>
2015-08-15 17:26:52 +0800

    图片显示不正常，调试

--- public/static/scripts/draft.js
+++ public/static/scripts/draft.js
@@ -129,7 +129,7 @@
         var localIds = res.localIds;  // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
 
         $("#preview").attr("src", localIds[0]);
-        $("#copyright").val(image.localIds[0])
+        $("#copyright").val(localIds[0])
         wx.uploadImage({
           localId: localIds[0],       // 需要上传的图片的本地ID，由chooseImage接口获得
           isShowProgressTips: 1,      // 默认为1，显示进度提示
