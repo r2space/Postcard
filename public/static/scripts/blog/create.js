984746353bef22f7e36ada7a6acc1d798bba9915
LI LIN <r2space@gmail.com>
2016-02-12 20:39:16 +0800

    添加博客功能

---
+++ public/static/scripts/blog/create.js
@@ -0,0 +1,20 @@
+(function ($) {
+  "use strict";
+
+  var id = $("#id").val()
+    , error = $("#error")
+    , errorMsg = $("#errorMsg")
+    , image = {};
+
+  /**
+   * 初始化
+   */
+  function init() {
+
+    error.hide();
+
+  }
+
+  init();
+
+}(jQuery));
